b81a7e559279b6451354e30f6d238f01a4a7b922
Amna <aisharif@qatar.cmu.edu>
2016-11-23 10:50:20 +0300

    mapcontrolleruncommentedRenderJson

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -3,13 +3,14 @@ class MapController < ApplicationController
 		# @rfid_tags = current_user.rfid_tags
 		#@rfid_tags =Detection.find(:all,:include => :lending)
 		@rfid_tags = Detection.joins(:lending)
+		render :json => @rfid_tags
 	end
 	def show
 		#@rfid_tags =Detection.find(:all,:include => :lending)
 		@rfid_tags = Detection.joins(:lending)
 		#@rfid_tags_json = render_to_string( formats: 'json' )
 		# format.json { render json: @rfid_tags, status: :unprocessable_entity }
-		#render :json => @rfid_tags
+		render :json => @rfid_tags
 	end
 
 
