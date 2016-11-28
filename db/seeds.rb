fb68e676fc217ccad31945b1a78b05f9c0895ab1
Amna <aisharif@qatar.cmu.edu>
2016-11-28 08:43:28 +0300

    MadeDesignChanges

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-users = User.create({email:"admin@admin.com",password:"admin1", admin:true})
-users = User.create({email:"user@user.com",password:"user1",name:"Amna",phone:"55240476", admin:false})
-users = User.create({email:"a@user.com",password:"user1",name:'Amna',phone:"55240476", admin:false})
+users = User.create({email:"admin@admin.com",password:"admin1",name:"Admin", admin:true, id: 1} )
+sensors = Sensor.create({sensor_id: "1"})
+rfid_tags = RfidTag.create({tag_id: "1"})
+lendings = Lending.create({tag_id: "1", user_id: 1})
+detections = Detection.create ({tag_id: "1", sensor_id: "1"})
