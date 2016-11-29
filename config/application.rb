fd6e769126f0c3501c027e3e330cd479bac0039e
Amna <aisharif@qatar.cmu.edu>
2016-11-29 09:36:21 +0300

    MadeChangestoApplicatio.rb

--- config/application.rb
+++ config/application.rb
@@ -5,9 +5,17 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+config.action_dispatch.default_headers = {
+    'Access-Control-Allow-Origin' => 'https://neverlost.herokuapp.com/map/show',
+    'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+  }
 
 module Workspace
   class Application < Rails::Application
+    config.action_dispatch.default_headers = {
+    'Access-Control-Allow-Origin' => 'https://neverlost.herokuapp.com/map/show',
+    'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+  }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
