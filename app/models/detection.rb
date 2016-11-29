03b0d5aab3bb3a3f02b5e080bc0ab8d4e46193b5
Amna <aisharif@qatar.cmu.edu>
2016-11-29 11:50:10 +0300

    SearchFeatureInDetectionWorks

--- app/models/detection.rb
+++ app/models/detection.rb
@@ -4,10 +4,6 @@ class Detection < ActiveRecord::Base
 	has_many :lendings, through: :rfid_tag, foreign_key: "tag_id"
 	#validates :sensor_id, presence: true
 	def self.search(search)
-  		if search
-   			 where('tag_id LIKE ?', "#{search}")
-  		else
-    		all
- 		end
+  		Detection.find_by_sql("SELECT * from rfid_tags, detections, lendings, users where detections.tag_id = rfid_tags.tag_id AND rfid_tags.tag_id = lendings.tag_id AND lendings.user_id = users.id  AND users.phone = #{search}")
 	end
 end
--- app/models/rfid_tag.rb
+++ app/models/rfid_tag.rb
@@ -2,9 +2,8 @@ class RfidTag < ActiveRecord::Base
 	has_many :detections, foreign_key: "tag_id"
 	has_many :sensors, :through => :detections
 	has_many :users, :through => :lendings
-	has_many :lendings, :class_name => 'User', :foreign_key => 'user_id'
-	has_many :lendings, :class_name => 'RfidTag', :foreign_key =>'tag_id'
+	has_many :lendings, :foreign_key =>'tag_id'
 	validates  :tag_id, :uniqueness => { :scope => :tag_id,
-    :message => "already registred" }
+    :message => "already registered" }
     validates :tag_id, presence: true
 end
