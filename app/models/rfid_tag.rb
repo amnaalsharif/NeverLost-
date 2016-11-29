f1e44bb7e11b9fccc6c96a2312c9e753daac035d
Amna <aisharif@qatar.cmu.edu>
2016-11-29 14:30:15 +0300

    FixedDetectionerror

--- app/models/rfid_tag.rb
+++ app/models/rfid_tag.rb
@@ -1,8 +1,9 @@
 class RfidTag < ActiveRecord::Base
+	self.primary_key = 'tag_id'
 	has_many :detections, foreign_key: "tag_id"
 	has_many :sensors, :through => :detections
 	has_many :users, :through => :lendings
-	has_many :lendings, :foreign_key =>'tag_id'
+	has_many :lendings, foreign_key:"tag_id"
 	validates  :tag_id, :uniqueness => { :scope => :tag_id,
     :message => "already registered" }
     validates :tag_id, presence: true
