7949b088cae5559eb2b9fb846aca634ff59e3bab
Amna <aisharif@qatar.cmu.edu>
2016-11-29 09:51:53 +0300

    ChangedinMapindexHttps

--- config/application.rb
+++ config/application.rb
@@ -10,10 +10,7 @@ Bundler.require(*Rails.groups)
 
 module Workspace
   class Application < Rails::Application
-    config.action_dispatch.default_headers = {
-    'Access-Control-Allow-Origin' => 'https://neverlost.herokuapp.com/map/show',
-    'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
-  }
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -29,6 +26,11 @@ module Workspace
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+        config.action_dispatch.default_headers = {
+    'Access-Control-Allow-Origin' => 'https://neverlost.herokuapp.com/map/show',
+    'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+  }
     
   end
 end
